bc71c3a22fba944e7dd2fdaa4b0cc979ab419ba4
Spirillen <pdns@protonmail.com>
2020-08-25 14:42:39 +0200

    first travis test

---
+++ miniconda/setup_miniconda.sh
@@ -0,0 +1,68 @@
+#!/usr/bin/env bash
+
+# Version options:
+# pyfunceble
+# pyfunceble-dev
+# PyFunceble@raw2sqlalchemy
+
+Pyfunceble_version="PyFunceble@raw2sqlalchemy"
+
+export PATH="${HOME}/miniconda/bin:${PATH}"
+
+if [[ "${TRAVIS_OS_NAME}" == "osx" ]]
+then
+	wget 'https://repo.anaconda.com/miniconda/Miniconda3-latest-MacOSX-x86_64.sh' \
+	  -O miniconda.sh
+else
+	wget 'https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh' \
+	  -O miniconda.sh
+fi
+
+bash miniconda.sh -b -p ${HOME}/miniconda
+hash -r
+conda config --set always_yes yes --set changeps1 no
+conda config --add channels conda-forge
+conda update -q conda
+conda create -q -n test-environment python="${PYTHON_VERSION}"
+source activate test-environment
+python --version --version
+pip --version
+conda --version
+pip uninstall -y pyfunceble pyfunceble-dev PyFunceble@raw2sqlalchemy
+pip install --no-cache-dir --upgrade dnspython[dnssec,idna]
+pip install --no-cache-dir -U "git+https://github.com/funilrys/Pyfunceble_version"
+# pip install --no-cache-dir --upgrade pyfunceble-dev
+# pip install git+https://github.com/funilrys/PyFunceble.git@dev
+
+rm -f miniconda.sh
+
+## We do not install ultimate-hosts-blacklist-whitelist(-dev) as we like
+## to know everything. And whitelisting should be set as user defined
+## desires via categorizing:
+## https://github.com/Ultimate-Hosts-Blacklist/dev-center/issues/50#issuecomment-667511820
+#
+## All whitelisting should also be a individual choice, never from a
+## central predefined decision. That is democracy.
+
+
+exit ${?}
+
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the modified GNU Affero General Public License as
+# published by the My Privacy DNS, either version 3 of the
+# License, or any later version released at
+# https://www.mypdns.org/w/License.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# modified GNU Affero General Public License for more details.
+
+# You should have received a copy of the modified GNU Affero General Public License
+# along with this program. If not, see https://www.mypdns.org/w/License.
+
+# The modification: The standard AGPLv3 have been altered to NOT allow
+# any to generate profit from our work. You are however free to use it to any
+# NON PROFIT purpose. If you would like to use any of our code for profiteering
+# YOU are obliged to contact https://www.mypdns.org/ for profit sharing.
---
+++ travis/setup_travis.sh
@@ -0,0 +1,96 @@
+#!/usr/bin/env bash
+
+# Don't use loop or array for simplicity, it will allow a broader
+# ordinance to contribute.
+
+# This script is to install requirements for running on Travis-CI
+
+# Variables
+git_dir="$(git rev-parse --show-toplevel)"
+
+# Get scripts
+
+## The PyFunceble test script
+## https://www.mypdns.org/P13
+wget -qO- "${git_dir}/dns-test.sh" \
+  "https://www.mypdns.com/file/data/xncc6jx357z44smp4mwx/PHID-FILE-n47dr7m37udxsa73tpz2/pyfunceble_dns-test.sh"
+  #"${SCRIPT_SOURCE_URI}/pyfunceble/dns-test.sh"
+
+## The Recursor setup script
+## https://www.mypdns.org/P12
+wget -qO- "${git_dir}/setup_recursor.sh" \
+  "https://www.mypdns.com/file/data/mrkuy7efq4it5uupqcg7/PHID-FILE-i6n4bygwx24vy2z2kmga/recursor_setup_recursor.sh"
+  #"${SCRIPT_SOURCE_URI}/recursor/setup_recursor.sh"
+
+## The Miniconda setup script
+## https://www.mypdns.org/P11
+wget -qO- "${git_dir}/setup_miniconda.sh" \
+  "https://www.mypdns.com/file/data/qr7i7tnze2ifuogxd7aj/PHID-FILE-m5d2unz3aq2glpleb6hc/miniconda_setup_miniconda.sh"
+  #"${SCRIPT_SOURCE_URI}/miniconda/setup_miniconda.sh"
+
+## Get requirements
+wget -qO- "${git_dir}/requirements.txt" \
+  "${SCRIPT_SOURCE_URI}/requirements.txt"
+
+
+# Set chmod +x on scripts
+## The PyFunceble test script
+if [ -f "${git_dir}/dns-test.sh" ]
+then
+  chmod +x "${git_dir}/dns-test.sh"
+else
+	echo "The script {0} have failed to download"
+	exit 2
+fi
+
+## The Recursor setup script
+if [ -f "${git_dir}/setup_recursor.sh" ]
+then
+  chmod +x "${git_dir}/setup_recursor.sh"
+else
+	echo "The script {0} have failed to download"
+	exit 2
+fi
+
+## The Miniconda setup script
+if [ -f "${git_dir}/setup_miniconda.sh" ]
+then
+  chmod +x "${git_dir}/setup_miniconda.sh"
+else
+	echo "The script {0} have failed to download"
+	exit 2
+fi
+
+
+# Run Scripts
+## Order matters!!!
+
+### Install MiniConda
+bash "${git_dir}/setup_miniconda.sh"
+
+### Install the Recursor
+bash "${git_dir}/setup_recursor.sh"
+
+
+
+exit ${?}
+
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the modified GNU Affero General Public License as
+# published by the My Privacy DNS, either version 3 of the
+# License, or any later version released at
+# https://www.mypdns.org/w/License.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# modified GNU Affero General Public License for more details.
+
+# You should have received a copy of the modified GNU Affero General Public License
+# along with this program. If not, see https://www.mypdns.org/w/License.
+
+# The modification: The standard AGPLv3 have been altered to NOT allow
+# any to generate profit from our work. You are however free to use it to any
+# NON PROFIT purpose. If you would like to use any of our code for profiteering
+# YOU are obliged to contact https://www.mypdns.org/ for profit sharing.
