087a8c626daf7963589f2c60c13474de7d3288c8
spirillen <44526987+spirillen@users.noreply.github.com>
2020-10-04 15:58:52 +0200

    Disabled quiet to see what is happening

--- pyfunceble/dns-test.sh
+++ pyfunceble/dns-test.sh
@@ -24,7 +24,7 @@ echo ""
 pyfunceble -f 'https://raw.githubusercontent.com/mypdns/matrix/master/source/porno-sites/wildcard.list.old' \
   --dns 127.0.0.1:5300 -h -a --idna -nf -ex --hierarchical -m \
   -db --database-type mariadb -dbr 0 --share-logs --dots --complements \
-  --ci -q --autosave-minutes 5 --ci-branch "${TRAVIS_BRANCH}" \
+  --ci --autosave-minutes 5 --ci-branch "${TRAVIS_BRANCH}" \
   --commit-autosave-message "www.mypDNS.org Test HUB Travis Run ID ${TRAVIS_BUILD_NUMBER} [Auto Saved]" \
   --commit-results-message "www.mypDNS.org Test HUB Travis Run ID ${TRAVIS_BUILD_NUMBER}"
   --cmd "'echo "${TRAVIS_JOB_ID}" > "${TRAVIS_BUILD_DIR}/run_id"'"
